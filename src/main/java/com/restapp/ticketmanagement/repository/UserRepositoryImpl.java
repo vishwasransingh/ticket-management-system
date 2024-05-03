c906c35f48fb8aacfc63dd4ee2baf053d83afb2c
vishwasransing <vishwasransing@gmail.com>
2024-05-03 15:27:29 +0530

    Create UserRepositoryImpl.java

---
+++ src/main/java/com/restapp/ticketmanagement/repository/UserRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.restapp.ticketmanagement.repository;
+
+import java.util.Map;
+
+import com.restapp.ticketmanagement.pojo.User;
+
+import reactor.core.publisher.Flux;
+
+public class UserRepositoryImpl implements UserRepository {
+	
+	Map<Integer, User> users;
+
+	public UserRepositoryImpl() {
+		users = Map.of(
+				1, (new User(1, "Vishwas")),
+				2, (new User(2, "Satu")));
+	}
+
+	@Override
+	public Flux<User> getAllUsers() {
+		return Flux.fromIterable(this.users.values());
+	}
+
+}
